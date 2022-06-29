50a1f6ccc78eaa2fed60f883cae23278639888ab
Hanif Jameel <hanif.jameel@gmail.com>
2022-06-29 21:22:56 +0100

    initial

---
+++ simple_jira_extractor.py
@@ -0,0 +1,14 @@
+from jira import JIRA
+import json
+
+with open('jira_config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+jira = JIRA(
+    options={'server': config['server']},
+    basic_auth=(config['email'], config['apitoken']))
+
+epics = jira.search_issues("project=TEAM AND issuetype=Epic")
+
+for epic in epics:
+    print(epic.key, epic.fields.status, epic.fields.customfield_10026)
